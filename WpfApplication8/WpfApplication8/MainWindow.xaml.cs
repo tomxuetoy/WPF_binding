9c217427e83f5c8e47b3b4ef36b65aec4f32b011
Tom Xue <tomxue0126@gmail.com>
2013-11-14 00:20:20 +0800

    start point, but not run correctly

---
+++ WpfApplication8/WpfApplication8/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string str = "initial...";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var t = new DispatcherTimer();
+            // 1 second
+            t.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            t.Tick += aaa;
+            t.IsEnabled = true;
+            t.Start();
+
+            DataContext = this;
+        }
+
+        private void aaa(object sender, EventArgs e)
+        {
+            for (int i = 0; ; i++)
+            {
+                Names = i.ToString();
+            }
+        }
+
+        public string Names
+        {
+            get { return str; }
+            set { str = value; }
+        }
+    }
+}
